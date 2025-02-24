4730467afc54ccdb57c143deb24aad82b67667f7
Михаил <shonovgacha@gmail.com>
2025-02-24 07:29:32 +0300

    fix date bug

--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@ const dataText = document.querySelector('.weather-card-text')
 const arrowLeft = document.querySelector('.left')
 const arrowRight = document.querySelector('.right')
 const apiKey = 'd8c6141a21074bfbad0120614252102'
+let dayCount = 0
 
+// ПОИСК ПОГОДЫ ПО ГОРОДУ
 async function checkWeather() {
     try {
         const loading = document.querySelector('.loading')
@@ -19,16 +21,18 @@ async function checkWeather() {
         weatherContent.classList.add('hidden')
 
         const cityName = weatherInput.value.trim()
+
         const weatherApi = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=7`
         const response = await fetch(weatherApi)
         const data = await response.json()
+        console.log(data)
 
         if (!data.forecast) {
             throw new Error('Неверный ответ от API')
         }
 
-        const forecastArr = data.current
-        const temp = forecastArr.temp_c
+        const currentArr = data.current
+        const temp = currentArr.temp_c
 
         weatherTitle.textContent = data.location.name
         weatherTemp.textContent = `${Math.round(temp)}℃`
@@ -44,6 +48,82 @@ async function checkWeather() {
             dataText.textContent = `${dateDay}.${dateMouth}.${dateYear}`
         }
 
+        const weatherCode = currentArr.condition.code
+        console.log(weatherCode)
+        if (weatherCode === 1000) {
+            // Солнечно
+        } else if (
+            weatherCode === 1003 ||
+            weatherCode === 1006 ||
+            weatherCode === 1009 ||
+            weatherCode === 1030
+        ) {
+            // облочно
+        } else if (
+            weatherCode === 1063 ||
+            weatherCode === 1198 ||
+            weatherCode === 1150 ||
+            weatherCode === 1153 ||
+            weatherCode === 1180 ||
+            weatherCode === 1183 ||
+            weatherCode === 1186 ||
+            weatherCode === 1189 ||
+            weatherCode === 1198 ||
+            weatherCode === 1240 ||
+            weatherCode === 1243
+        ) {
+            // weatherImg.setAttribute('src', './img/')
+            // небольшой дождь
+        } else if (
+            weatherCode === 1192 ||
+            weatherCode === 1195 ||
+            weatherCode === 1246
+        ) {
+            // дождь
+        } else if (
+            weatherCode === 1066 ||
+            weatherCode === 1168 ||
+            weatherCode === 1171 ||
+            weatherCode === 1201 ||
+            weatherCode === 1204 ||
+            weatherCode === 1210 ||
+            weatherCode === 1213
+        ) {
+            // небольшой снег
+        } else if (
+            weatherCode === 1114 ||
+            weatherCode === 1117 ||
+            weatherCode === 1207 ||
+            weatherCode === 1216 ||
+            weatherCode === 1219 ||
+            weatherCode === 1222 ||
+            weatherCode === 1225 ||
+            weatherCode === 1237 ||
+            weatherCode === 1258
+        ) {
+            // снег
+        } else if (
+            weatherCode === 1069 ||
+            weatherCode === 1072 ||
+            weatherCode === 1252 ||
+            weatherCode === 1249 ||
+            weatherCode === 1255 ||
+            weatherCode === 1261 ||
+            weatherCode === 1264
+        ) {
+            // снег с дождём
+        } else if (weatherCode === 1087) {
+            // гроза
+        } else if (
+            weatherCode === 1273 ||
+            weatherCode === 1276 ||
+            weatherCode === 1279 ||
+            weatherCode === 1282
+        ) {
+            // гроза с дождём
+        } else if (weatherCode === 1135 || weatherCode === 1147) {
+            // туман
+        }
         setTimeout(() => {
             document
             weatherContent.classList.remove('hidden')
@@ -72,6 +152,7 @@ seacrchBtn.addEventListener('click', function () {
 
 weatherInput.addEventListener('keydown', handleEvent)
 
+// СТРЕЛКИ ВПРАВО И ВЛЕВО
 const arrOfDay = [
     'сегодня',
     'завтра',
@@ -81,7 +162,6 @@ const arrOfDay = [
     'через 5 дней',
     'через 6 дней',
 ]
-let dayCount = 0
 let date = new Date()
 let dateDay = date.getDate()
 let dateMouth = date.getMonth() + 1
@@ -92,16 +172,15 @@ async function goRight() {
         dayCount++
     }
     dataTitle.textContent = arrOfDay[dayCount]
-    if (dateMouth < 10) {
-        dataText.textContent = `${dateDay + dayCount}.0${dateMouth}.${dateYear}`
-    } else {
-        dataText.textContent = `${dateDay + dayCount}.${dateMouth}.${dateYear}`
-    }
 
     const cityName = weatherTitle.textContent
     const weatherApi = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=7`
     const response = await fetch(weatherApi)
     const data = await response.json()
+    const dataDay = data.forecast.forecastday[dayCount].date
+
+    dataText.textContent = dataDay.split('-').reverse().join('.')
+
     if (dayCount === 0) {
         const forecastArr = data.current
         const temp = forecastArr.temp_c
@@ -121,15 +200,14 @@ async function goLeft() {
         dayCount--
     }
     dataTitle.textContent = arrOfDay[dayCount]
-    if (dateMouth < 10) {
-        dataText.textContent = `${dateDay + dayCount}.0${dateMouth}.${dateYear}`
-    } else {
-        dataText.textContent = `${dateDay + dayCount}.${dateMouth}.${dateYear}`
-    }
+
     const cityName = weatherTitle.textContent
     const weatherApi = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=7`
     const response = await fetch(weatherApi)
     const data = await response.json()
+    const dataDay = data.forecast.forecastday[dayCount].date
+
+    dataText.textContent = dataDay.split('-').reverse().join('.')
 
     if (dayCount === 0) {
         const forecastArr = data.current
